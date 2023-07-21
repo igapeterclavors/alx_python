4acee617aff49da7cb1d9beb7ad1351e7a568789
piclavors <iga.peterclavors@gmail.com>
2023-07-21 03:38:31 -0700

    task 3

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -11,8 +11,8 @@ else:
     last_digit = new % 10
 
 if lastDigit > 5:
-    print("Last digit is greater than 5".format(number, last_digit))
+    print("Last digit  of {} is {} and is greater than 5".format(number, last_digit))
 elif lastDigit < 0:
-    print("Last digit is less than 6 and not 0".format(number, last_digit))
+    print("Last digit  of {} is {} and is less than 6 and not 0".format(number, last_digit))
 else:
-    print("Last digit is 0".format(number, last_digit))
+    print("Last digit  of {} is {} and is 0".format(number, last_digit))
