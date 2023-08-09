f3fc86ec17866a03544f13a99807ff1c4e5218f8
igapeterclavors <iga.peterclavors@gmail.com>
2023-08-09 18:58:15 +0300

    adding task 1-print_matrix_integer.py of python datastructures

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -11,14 +11,4 @@ def print_matrix_integer(matrix=[[]]):
                 print("{:d}".format(column_element), end=" ")
 
             # Move to the next line after each row
-        print()  
-
-
-# matrix = [
-#     [1, 2, 3],
-#     [4, 5, 6],
-#     [7, 8, 9]
-# ]
-# print_matrix_integer(matrix)
-# print("--")
-# print_matrix_integer()
+        print()
