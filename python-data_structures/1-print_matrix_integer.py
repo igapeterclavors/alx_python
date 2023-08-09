b4307c33bfe05de3c25fd4bda880484cffc474ec
igapeterclavors <iga.peterclavors@gmail.com>
2023-08-09 20:57:12 +0300

    2-multiple_returns.py task 2 of python data structures

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -8,11 +8,11 @@ def print_matrix_integer(matrix=[[]]):
         print()  
 
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
+# matrix = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
+# print_matrix_integer(matrix)
+# print("--")
+# print_matrix_integer()
