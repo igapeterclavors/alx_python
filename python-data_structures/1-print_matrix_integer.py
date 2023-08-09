207cb1558255756ad951ab9475b5a2159e889b2b
igapeterclavors <iga.peterclavors@gmail.com>
2023-08-09 21:07:23 +0300

    2-multiple_returns.py task 2 of python data structures

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,18 +1,20 @@
 # Write a function that prints a matrix of integers.
 
 def print_matrix_integer(matrix=[[]]):
-    for row_element in matrix:
-        for column_element in row_element:
-            print("{:d}".format(column_element), end=" ")
-            # Move to the next line after each row
-        print()  
+    for row in matrix:
+        for column in row:
+            if column != row[-1]:
+                print('{:d}'.format(column), end=" ")
+            else:
+                print('{:d}'.format(column), end="")
+        print()
 
 
-# matrix = [
-#     [1, 2, 3],
-#     [4, 5, 6],
-#     [7, 8, 9]
-# ]
-# print_matrix_integer(matrix)
-# print("--")
-# print_matrix_integer()
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
