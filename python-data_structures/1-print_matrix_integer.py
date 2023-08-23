0277dfa03a98b2cc231ece19257fa2662ef797d6
igapeterclavors <iga.peterclavors@gmail.com>
2023-08-23 21:46:56 +0300

    adding tasks 1

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,17 +1,11 @@
-# Write a function that prints a matrix of integers. 
+# Write a function that prints a matrix of integers.
 
 def print_matrix_integer(matrix=[[]]):
     for row_element in matrix:
-        i = 1
-        if len(row_element) == 0:
-            print("")
-        else:
-            for column_element in row_element:
-                if i < len(row_element):
-                    print("{:d}".format(column_element), end=" ")
-                else:
-                    print("{:d}".format(column_element), end="\n")
-                i += 1
+        for column_element in row_element:
+            print("{:d}".format(column_element), end=" ")
+        print()  
+
 
 # matrix = [
 #     [1, 2, 3],
