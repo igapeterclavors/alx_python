198a6870ce3691c338c5bb7b00d339ac6e420506
igapeterclavors <iga.peterclavors@gmail.com>
2023-08-30 12:11:22 +0300

    editing comb3

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -10,6 +10,5 @@ for x in range(9):
     for y in range(1, 10):
         if (x == 8 and y == 9):
             print("{}{}".format(x, y))
-
         elif (y != x and x < y):
             print("{}{}".format(x, y), end=", ")
