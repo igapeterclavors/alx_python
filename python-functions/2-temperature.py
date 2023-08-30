44699caf4b62e422ecf04e2bfc05b1738aaa66b6
igapeterclavors <iga.peterclavors@gmail.com>
2023-08-30 13:34:07 +0300

    editing temperature.py, and prime.py

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,5 +1,11 @@
-#!/usr/bin/env python3
+# function takes a temperature in Fahrenheit as input and returns the temperature in Celsius.
 
 def convert_to_celsius(fahrenheit):
-    celsius = 5/9 * (fahrenheit - 32)
+    celsius = (fahrenheit - 32) * 5 / 9
     return celsius
+
+
+# print(convert_to_celsius(100))
+# print(convert_to_celsius(-40))
+# print(convert_to_celsius(-459.67))
+# print(convert_to_celsius(32))
--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,15 +1,35 @@
 #!/usr/bin/env python3
 
-def is_prime(number):
-    if number == 0:
-            return False
+# def is_prime(number):
+#     if number == 0:
+#             return False
     
-    elif number < 0:
-            return False
-    else:
-        for i in range(2, number):
+#     elif number < 0:
+#             return False
+#     else:
+#         for i in range(2, number):
             
-                if number % i == 0:
-                        return False
-                        break
-    return  True
+#                 if number % i == 0:
+#                         return False
+#                         break
+#     return  True
+
+
+def is_prime(number):
+    if number <= 1:
+        return False
+    if number <= 3:
+        return True
+    if number % 2 == 0 or number % 3 == 0:
+        return False
+    i = 5
+    while i * i <= number:
+        if number % i == 0 or number % (i + 2) == 0:
+            return False
+        i += i
+    return True
+    
+# print(is_prime(17))
+# print(is_prime(15))
+# print(is_prime(-5))
+# print(is_prime(0))
