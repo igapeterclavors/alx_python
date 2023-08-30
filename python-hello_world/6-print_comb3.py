21926015b2418fc9868511f4046366dd4c9867d9
igapeterclavors <iga.peterclavors@gmail.com>
2023-08-30 12:46:51 +0300

    editing comb3

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,11 +1,4 @@
-# for i in range(1, 10):
-#     for j in range(i + 1, 0):
-#         print("{:02d}, ".format(i * 10 + j), end="")
-#         print()
-
-
 #prints all possible different combinations of two digits
-
 for x in range(9):
     for y in range(1, 10):
         if (x == 8 and y == 9):
